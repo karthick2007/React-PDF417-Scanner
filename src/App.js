297f3f6fde169995d56100c01dbb8a2a8e1489fb
karthick2007 <36950376+karthick2007@users.noreply.github.com>
2020-02-29 20:01:50 -0800

    made changes to camera position

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Camera, {FACING_MODES} from 'react-html5-camera-photo';
 import 'react-html5-camera-photo/build/css/index.css';
 
 class App extends Component {
 
-
-  
-
     state= {
       imgSrc : '',
       firstName:''
     }
-    
- 
 
   scan(){
 
-    try{
-      let driverData = window.doScan(document.querySelector('img'));
+        try{
+          let driverData = window.doScan(document.querySelector('img'));
 
-    if(driverData !== undefined){
-      this.setState({
-        firstName:driverData.firstName
-      })
-    }
-    
+            if(driverData !== undefined){
+              this.setState({
+                firstName:driverData.firstName
+              })
+            }
+            
 
-    if(JSON.stringify(driverData) === '{}'){
-      throw new Error('Error');
-    }
-  }catch(err){
-    let errorElement = document.getElementById('error');
-    errorElement.innerHTML = "Error in scanning";
-    errorElement.style.display = 'inline';
-    errorElement.style.color = 'red';
-    }
+            if(JSON.stringify(driverData) === '{}'){
+              throw new Error('Error');
+            }
+      }catch(err){
+        let errorElement = document.getElementById('error');
+        errorElement.innerHTML = "Error in scanning";
+        errorElement.style.display = 'inline';
+        errorElement.style.color = 'red';
+        }
 
     }
     
@@ -63,26 +57,31 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          
+
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <input type="file" id="myFile" onChange = {this.handleFiles.bind(this)}></input>
-        <label id = "error" style = {{display:"none"}}></label>
-      <img onLoad={this.scan.bind(this)} style={{display:'none'}} src = {this.state.imgSrc} alt = ''>
-    
-      </img>
-      <p>{this.state.firstName}</p>
-      <Camera
-        idealFacingMode = {FACING_MODES.ENVIRONMENT}
-        isImageMirror = {false}
-        isMaxResolution = {true}
-      onTakePhoto={ (dataUri) => this.handleTakePhoto(dataUri)
-      }
-     
-    />
+        <input type="file" id="myFile" onChange={this.handleFiles.bind(this)}></input>
+        <label id="error" style={{ display: "none" }}></label>
+        <img onLoad={this.scan.bind(this)} style={{ display: 'none' }} src={this.state.imgSrc} alt=''>
+
+        </img>
+        <p>{this.state.firstName}</p>
+        <div style = {{display:'flex',justifyContent:'space-between'}}>
+        <button style = {{width:'150px'}}></button>
+         <Camera 
+          idealFacingMode={FACING_MODES.ENVIRONMENT}
+          isImageMirror={false}
+          isMaxResolution={true}
+          isFullscreen = {false }
+          idealResolution = {{width: 640, height: 480}}
+          onTakePhoto={(dataUri) => this.handleTakePhoto(dataUri)
+          }/>
+     <button style = {{width:'150px'}}></button>
+         
+         </div>  
       </div>
     );
   }
